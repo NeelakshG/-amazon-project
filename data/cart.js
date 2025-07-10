b67dc0d96a329c92202d7e175f8670de939d1711
NeelakshG <neelaksh.gupta05@gmail.com>
2025-07-09 22:18:05 -0400

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export const cart = [];
+//we are normalizing the data, instead of adding every feature, we are going to search through the products using the id
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
