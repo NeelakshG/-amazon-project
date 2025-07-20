72ec4a4086f8b5b4e7db6b95903d7efac6c873b5
NeelakshG <neelaksh.gupta05@gmail.com>
2025-07-19 22:48:47 -0400

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,13 +1,14 @@
 import { cart, addToCart } from "../data/cart.js";
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+loadProducts(renderProductsGrid); //will send a request, but it takes time so we need to wait for the response to load
 // modules help with naming conflict,
 let productsHTML = "";
 
-//dynamically creating a template for each product, we use the product array and take in whatever data we need ot take
-products.forEach((product) => {
-  productsHTML += `
+function renderProductsGrid() {
+  products.forEach((product) => {
+    productsHTML += `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
@@ -60,30 +61,33 @@ products.forEach((product) => {
       </button>
     </div>
   `;
-});
-
-//after adding each product, we add all of this into the grid
-document.querySelector(".js-products-grid").innerHTML = productsHTML;
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  //getting the cartQuantity by going through the cart and adding each value of quantity
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
   });
-  //now we update the website by setting the html to whatever value we get
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+  //after adding each product, we add all of this into the grid
+  document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    //getting the cartQuantity by going through the cart and adding each value of quantity
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+    //now we update the website by setting the html to whatever value we get
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
+
+  //to ensure this happens, we listen to whenever user clicks the addCart button, when thats clicked, we retrieve the id and call a function while
+  //simoulensouly update the cart
+  document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId; //retrieves the data value of product-id in the html
+      ``;
+
+      addToCart(productId); //add it to the cart
+      updateCartQuantity(); //update the cart quantity
+    });
+  });
 }
 
-//to ensure this happens, we listen to whenever user clicks the addCart button, when thats clicked, we retrieve the id and call a function while
-//simoulensouly update the cart
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId; //retrieves the data value of product-id in the html
-    ``;
-
-    addToCart(productId); //add it to the cart
-    updateCartQuantity(); //update the cart quantity
-  });
-});
+//dynamically creating a template for each product, we use the product array and take in whatever data we need ot take
